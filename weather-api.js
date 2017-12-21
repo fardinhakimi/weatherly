e4f11c62550c34ccc7ae265c8c57c7d6fd28ec46
fardin hakimi <fardinhakimi@gmail.com>
2017-12-21 05:01:07 +0100

    fixed webhook typo

--- weather-api.js
+++ weather-api.js
@@ -1,6 +1,6 @@
 module.exports = (app) => {
 
-    app.get('/test-webhook', (req, res) => {
+    app.post('/test-webhook', (req, res) => {
 
         response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
         res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
